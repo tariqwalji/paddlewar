6d645c89d8ed819895449260b691b3e7cc15c42e
tariqwalji <tariqwalji@yahoo.co.uk>
2020-04-20 23:34:27 +0100

    Added ball test

---
+++ test/ball.js
@@ -0,0 +1,66 @@
+const assert = require("assert");
+const expect = require("chai").expect;
+
+const ball = {
+    x: 0,
+    y: 0,
+    width: 5,
+    height: 5,
+
+    moveLeft(step) {
+        this.x -= step;
+    },
+
+    moveRight(step) {
+        this.x += step;
+    },
+
+    moveUp(step) {
+        this.y -= step;
+    },
+
+    moveDown(step) {
+        this.y += step;
+    }
+};
+
+describe("ball", () => {
+    beforeEach(() => {
+        ball.x = 0;
+        ball.y = 0;
+    });
+
+    it("exists", () => {
+        assert.equal(typeof ball, "object", "ball is an object");
+    });
+
+    it("has coordinates", () => {
+        expect(ball).to.have.own.property("x", 0, "x coordinate");
+        expect(ball).to.have.own.property("y", 0, "y coordinate");
+    });
+
+    it("can move left by 1", () => {
+        ball.moveLeft(1);
+        assert.equal(ball.x, -1, "ball x to be -1");
+    });
+
+    it("can move right by 1", () => {
+        ball.moveRight(1);
+        assert.equal(ball.x, 1, "ball x to be 1");
+    });
+
+    it("can move up by 1", () => {
+        ball.moveUp(1);
+        assert.equal(ball.y, -1, "ball y to be -1");
+    });
+
+    it("can move down by 1", () => {
+        ball.moveDown(1);
+        assert.equal(ball.y, 1, "ball y to be 1");
+    });
+
+    it("has has a dimension of 5x5", () => {
+        assert.equal(ball.width, 5, "ball width of 5");
+        assert.equal(ball.height, 5, "ball height of 5");
+    });
+});
